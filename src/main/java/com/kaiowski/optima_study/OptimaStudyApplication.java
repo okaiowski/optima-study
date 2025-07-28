fa0d64f89d620f49e9891063636629f88057ffe7
Kaio Henrique Roque Silva <kaioroque.dev@gmail.com>
2025-07-28 14:19:08 -0400

    Adding the src folder with subfolders.

---
+++ src/main/java/com/kaiowski/optima_study/OptimaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.kaiowski.optima_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimaStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaiowski/optima_study/OptimaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiowski.optima_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
